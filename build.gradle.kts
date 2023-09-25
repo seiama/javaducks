fcb1fc1e26da7548aac8ae4962add67b57247241
zml <zml@stellardrift.ca>
2023-09-24 22:39:27 -0700

    chore(build): Produce a functioning docker image

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,9 @@
+buildscript {
+  dependencies {
+    classpath("com.google.cloud.tools:jib-spring-boot-extension-gradle:0.1.0")
+  }
+}
+
 plugins {
   val indraVersion = "3.1.3"
   id("com.diffplug.spotless") version "6.21.0"
@@ -5,6 +11,7 @@ plugins {
   id("net.kyori.indra.checkstyle") version indraVersion
   id("io.spring.dependency-management") version "1.1.3"
   id("org.springframework.boot") version "3.1.4"
+  id("com.google.cloud.tools.jib") version "3.4.0"
 }
 
 group = "com.seiama"
@@ -33,6 +40,44 @@ spotless {
   }
 }
 
+jib {
+  to.image = "ghcr.io/seiama/javaducks/javaducks"
+  from {
+    image = "eclipse-temurin:${indra.javaVersions().target().get()}-jre"
+    platforms {
+      // We can only build multi-arch images when pushing to a registry, not when building locally
+      val requestedTasks = gradle.startParameter.taskNames
+      if ("jibBuildTar" in requestedTasks || "jibDockerBuild" in requestedTasks) {
+        platform {
+          // todo: better logic
+          architecture = when (System.getProperty("os.arch")) {
+            "aarch64" -> "arm64"
+            else -> "amd64"
+          }
+          os = "linux"
+        }
+      } else {
+        platform {
+          architecture = "amd64"
+          os = "linux"
+        }
+        platform {
+          architecture = "arm64"
+          os = "linux"
+        }
+      }
+    }
+  }
+  pluginExtensions {
+    pluginExtension {
+      implementation = "com.google.cloud.tools.jib.gradle.extension.springboot.JibSpringBootExtension"
+    }
+  }
+  container {
+    args = listOf("--spring.config.additional-location=optional:file:/config/")
+    ports = listOf("8080")
+  }
+}
 dependencies {
   annotationProcessor("org.springframework.boot", "spring-boot-configuration-processor")
   checkstyle("ca.stellardrift:stylecheck:0.2.1")
